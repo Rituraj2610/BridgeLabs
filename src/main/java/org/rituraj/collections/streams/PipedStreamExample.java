d05fb056f21aa0a81bf3bc81e068ca3a22c20ef3
Rituraj2610 <26rituraj@gmail.com>
2025-03-25 00:12:15 +0530

    [Rituraj] ADD . created java program that implements piped streams

---
+++ src/main/java/org/rituraj/collections/streams/PipedStreamExample.java
@@ -0,0 +1,16 @@
+package org.rituraj.collections.streams;
+import java.io.*;
+class PipedStreamExample {
+    public static void main(String[] args) throws IOException {
+        PipedInputStream pis = new PipedInputStream();
+        PipedOutputStream pos = new PipedOutputStream(pis);
+        new Thread(() -> {
+            try (pos) { pos.write("Hello from Writer".getBytes()); }
+            catch (IOException e) { e.printStackTrace(); }
+        }).start();
+        new Thread(() -> {
+            try (pis) { System.out.println(new String(pis.readAllBytes())); }
+            catch (IOException e) { e.printStackTrace(); }
+        }).start();
+    }
+}
