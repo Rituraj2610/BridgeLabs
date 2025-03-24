ffc6feadc91dfa32d9017596ab9fa899cd450abf
Rituraj2610 <26rituraj@gmail.com>
2025-03-25 00:11:07 +0530

    [Rituraj] ADD . created java program that read writes file

---
+++ src/main/java/org/rituraj/collections/streams/FileReadWrite.java
@@ -0,0 +1,24 @@
+package org.rituraj.collections.streams;
+
+import java.io.*;
+
+class FileReadWrite {
+    public static void main(String[] args) {
+        File inputFile = new File("source.txt");
+        File outputFile = new File("destination.txt");
+
+        try (FileInputStream fis = new FileInputStream(inputFile);
+             FileOutputStream fos = new FileOutputStream(outputFile)) {
+            int data;
+            while ((data = fis.read()) != -1) {
+                fos.write(data);
+            }
+            System.out.println("File copied successfully.");
+        } catch (FileNotFoundException e) {
+            System.out.println("Source file not found.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
