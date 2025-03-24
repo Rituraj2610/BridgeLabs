bcccba9afae9906318114d647100d9430d3f9191
Rituraj2610 <26rituraj@gmail.com>
2025-03-25 00:12:56 +0530

    [Rituraj] ADD . created java program that prints word count

---
+++ src/main/java/org/rituraj/collections/streams/WordCount.java
@@ -0,0 +1,21 @@
+package org.rituraj.collections.streams;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+class WordCount {
+    public static void main(String[] args) throws IOException {
+        Map<String, Integer> wordCount = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("text.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                for (String word : line.split("\\W+")) {
+                    wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+                }
+            }
+        }
+        wordCount.entrySet().stream().sorted((a, b) -> b.getValue() - a.getValue()).limit(5)
+                .forEach(entry -> System.out.println(entry.getKey() + " : " + entry.getValue()));
+    }
+}
+
